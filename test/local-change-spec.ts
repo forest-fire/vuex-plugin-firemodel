9890d7b884b7ce3a0664d2b730359411fa4269b1
Ken Snyder <ken@ken.net>
2019-06-27 11:17:05 -0700

    started to build tests

---
+++ test/local-change-spec.ts
@@ -0,0 +1,27 @@
+describe("local change effects @firemodel state", () => {
+  it('adding a new record results in addition to "onlyLocal"', async () => {
+    //
+  });
+
+  it('updating a new record results in addition to "onlyLocal"', async () => {
+    //
+  });
+
+  it('removing a new record results in addition to "onlyLocal"', async () => {
+    //
+  });
+});
+
+describe("local changes effect state in modules using fireModules", () => {
+  it("adding a new record results module state being changed appropriately", async () => {
+    //
+  });
+
+  it("updating a new record results module state being changed appropriately", async () => {
+    //
+  });
+
+  it("removing a new record results module state being changed appropriately", async () => {
+    //
+  });
+});
---
+++ test/store/userProfile.ts
@@ -0,0 +1,46 @@
+import { Module, GetterTree } from "vuex";
+import { IRootState } from "./index";
+import { firemodelMutations } from "../../src";
+
+export type IUserProfileState = {
+  name: {
+    first: string;
+    last: string;
+  };
+  restrictedByAge: boolean;
+  uid: string;
+  deliveryAddresses: {
+    [name: string]: {
+      address1: string;
+      address2: string;
+      city: string;
+      state: string;
+      zipcode: string;
+    };
+  };
+};
+
+export const state: IUserProfileState = {
+  name: {
+    first: "",
+    last: ""
+  },
+  restrictedByAge: process.env.NODE_ENV === "production",
+  uid: "",
+  deliveryAddresses: {}
+};
+
+export const getters: GetterTree<IUserProfileState, IRootState> = {
+  isRestrictedByAge() {
+    return state.restrictedByAge;
+  }
+};
+
+const userProfileModule: Module<IUserProfileState, IRootState> = {
+  state,
+  mutations: firemodelMutations(),
+  getters,
+  namespaced: true
+};
+
+export default userProfileModule;
