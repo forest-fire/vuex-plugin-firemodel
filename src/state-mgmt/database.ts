d4a69233b76738e9a6becd0a6afb95314d8cdb2b
Ken Snyder <ken@ken.net>
2020-06-02 20:10:30 -0700

    bumped u-f

--- src/state-mgmt/database.ts
+++ src/state-mgmt/database.ts
@@ -2,17 +2,17 @@ import type { FirestoreClient, IAbstractedDatabase, RealTimeClient } from 'unive
 
 import { FireModelPluginError } from "../private";
 
-let _db: FirestoreClient | RealTimeClient;
+let _db: IAbstractedDatabase;
 /**
  * provides access to the database that was passed in by the consuming application
  */
-export function getDatabase(): FirestoreClient | RealTimeClient {
+export function getDatabase(): IAbstractedDatabase {
   if(!_db) {
     throw new FireModelPluginError(`A call to database() failed because the database was not set!`)
   }
   return _db;
 }
 
-export function storeDatabase(db : FirestoreClient | RealTimeClient) {
+export function storeDatabase(db : IAbstractedDatabase) {
   _db = db
 }
