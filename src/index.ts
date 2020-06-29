4226992eb343f713e665083fb972639605e7098b
Ken Snyder <ken@ken.net>
2020-06-29 15:07:06 -0700

    pass through latest firemodel which now proxies fbKey

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // #autoindex:named, exclude: util, abc, auth, errors, firemodelMutations, store, types
+export * from "firemodel";
 //#region autoindexed files
 // indexed at: 6th Jun, 2020, 08:57 AM ( GMT-7 )
 // local file exports
