12e5385c66ac675fea67a84ce510d1e8aaa1458e
Ken Snyder <ken@ken.net>
2021-01-09 13:02:07 -0800

    broken build due to ttypescript's "afterDeclarations" transform

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,33 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript2 from "rollup-plugin-typescript2";
-import analyze from "rollup-plugin-analyzer";
-import pkg from "./package.json";
-import { builtinModules } from "module";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript2 from 'rollup-plugin-typescript2';
+import analyze from 'rollup-plugin-analyzer';
+import pkg from './package.json';
+import { builtinModules } from 'module';
 
-const generalConfig = moduleSystem => ({
-  input: "src/index.ts",
+const generalConfig = (moduleSystem) => ({
+  input: 'src/index.ts',
   output: {
     dir: `dist/${moduleSystem}`,
     format: `${moduleSystem}`,
-    sourcemap: false
+    sourcemap: false,
+    exports: 'named',
   },
   external: [
     ...Object.keys(pkg.dependencies),
     ...Object.keys(pkg.peerDependencies),
     ...Object.keys(pkg.optionalDependencies),
-    ...builtinModules
+    ...builtinModules,
   ],
   plugins: [
     commonjs(),
     resolve(),
     typescript2({
       tsconfig: `tsconfig.es.json`,
-      typescript: require("ttypescript")
+      typescript: require('ttypescript'),
     }),
-    ...(moduleSystem === "es" ? [analyze()] : [])
-  ]
+    ...(moduleSystem === 'es' ? [analyze()] : []),
+  ],
 });
 
-export default [generalConfig("cjs"), generalConfig("es")];
+export default [generalConfig('cjs'), generalConfig('es')];
